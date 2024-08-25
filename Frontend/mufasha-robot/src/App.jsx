34464bb9ad09eb4d7f85e4194bd65f9038d9a990
El-Shaddai3 <elshaddairukundo3@gmail.com>
2024-08-25 16:37:22 -0700

    Added a Landing page with the sign up form

---
+++ Frontend/mufasha-robot/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LandingPage from './components/LandingPage'
+
+export default function App() {
+  return (
+    <div className="app">
+      <div className="landing-page">
+        <LandingPage /> 
+      </div>
+    </div>
+  );
+}
---
+++ Frontend/mufasha-robot/src/components/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LandPage from '../pages/LandingPage/LandPage';
+
+export default function LandingPage() {
+    return(
+        <main style={{
+            backgroundColor: 'transparent'
+        }}>
+            <LandPage />
+        </main>
+    )
+}
